a563b3b1b93e5a93de561b4c6b59bc37ac82d708
lakshmi-webdesign <lakshmisiyer618@gmail.com>
2024-09-22 10:54:45 +0530

    added company api

--- BD6.5HW2/index.js
+++ BD6.5HW2/index.js
@@ -4,6 +4,8 @@ app.use(express.json());
 
 let employees = [];
 
+let companies = [];
+
 function validateEmployee(data) {
     if (!data.name || typeof data.name !== "string") {
         return "Name is required and should be a string"
@@ -11,6 +13,14 @@ function validateEmployee(data) {
 
     if (!data.companyId || typeof data.companyId !== "number") {
         return "CompanyId is required and should be number"
+    }
+
+    return null;
+}
+
+function validateCompany(data) {
+    if (!data.name || typeof data.name !== "string") {
+        return "Name is required and should be a string"
     }
 
     return null;
@@ -25,4 +35,13 @@ app.post("/api/employees", (req, res) => {
     res.status(201).json(employee);
 });
 
-module.exports = { app, validateEmployee };
+app.post("/api/companies", (req, res) => {
+    let error = validateCompany(req.body);
+    if (error) return res.status(400).send(error);
+
+    let company = { id: companies.length + 1, ...req.body };
+    companies.push(company);
+    res.status(201).json(company)
+})
+
+module.exports = { app, validateEmployee, validateCompany };
--- BD6.5HW2/test/api.test.js
+++ BD6.5HW2/test/api.test.js
@@ -1,4 +1,4 @@
-let { app, validateEmployee } = require("../index");
+let { app, validateEmployee, validateCompany } = require("../index");
 let http = require("http");
 let request = require("supertest");
 let server;
@@ -25,5 +25,58 @@ describe("api input validation", () => {
             name: 'John Doe',
             companyId: 1
         })
+    });
+
+    it("/api/employees should return 400 for invalid input", async () => {
+        let result = await request(server).post("/api/employees").send({
+            name: 'John Doe'
+        });
+
+        expect(result.status).toEqual(400);
+        expect(result.text).toEqual("CompanyId is required and should be number");
+    });
+
+    it("/api/companies should validate input correctly", async () => {
+        let result = await request(server).post("/api/companies").send({
+            name: 'TechCorp'
+        });
+
+        expect(result.status).toEqual(201);
+        expect(result.body).toEqual({
+            id: 1,
+            name: 'TechCorp'
+        });
+    })
+
+    it("/api/companies should return 400 for invlid input", async () => {
+        let result = await request(server).post("/api/companies").send({});
+
+        expect(result.status).toEqual(400);
+        expect(result.text).toEqual("Name is required and should be a string")
+    })
+})
+
+describe("validation functions", () => {
+    it("validateEmployee should validate input correctly", () => {
+        expect(validateEmployee({
+            name: 'John Doe',
+            companyId: 1
+        })).toBeNull();
+
+        expect(validateEmployee({
+            companyId: 1
+        })).toEqual("Name is required and should be a string");
+
+        expect(validateEmployee({
+            name: 'John Doe'
+        })).toEqual("CompanyId is required and should be number")
+    })
+
+    it("validateCompany should validate input correctly", () => {
+        expect(validateCompany({
+            name: 'TechCorp'
+        })).toBeNull();
+
+        expect(validateCompany({})).toEqual("Name is required and should be a string")
     })
 })
