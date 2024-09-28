ce9a2e662f63efee19ff6bc5f0e1ac41a1201caf
lakshmi-webdesign <lakshmisiyer618@gmail.com>
2024-09-28 20:35:30 +0530

    Added 6.6CW folder

---
+++ BD6.6CW/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+let app = express();
+let { getAllemployee, getEmployeeById } = require('./controller/index');
+
+app.get("/employees", async (req, res) => {
+    let result = await getAllemployee();
+    res.status(200).json(result);
+});
+
+app.get("/employees/details/:id", async (req, res) => {
+    let id = parseInt(req.params.id);
+    let result = await getEmployeeById(id);
+    if (!result) {
+        return res.status(404).send({ message: "Employee not found" })
+    }
+    res.status(200).json({employee: result});
+});
+
+module.exports = { app };
---
+++ BD6.6CW/test/api.test.js
@@ -0,0 +1,111 @@
+let http = require('http');
+let request = require('supertest');
+let { app } = require("../index")
+let { getAllemployee, getEmployeeById } = require("../controller/index");
+let server;
+
+jest.mock('../controller/index.js', () => ({
+    ...jest.requireActual('../controller/index'),
+    getAllemployee: jest.fn(),
+    getEmployeeById: jest.fn()
+}));
+
+beforeAll((done) => {
+    server = http.createServer(app);
+    server.listen(3000, done);
+});
+
+afterAll((done) => {
+    server.close(done);
+});
+
+describe("api test", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+    it("/employees should return all employeeData", async () => {
+        const mockData = {
+            employees: [
+                {
+                    employeeId: 1,
+                    name: 'Rahul Sharma',
+                    email: 'rahul.sharma@example.com',
+                    departmentId: 1,
+                    roleId: 1,
+                },
+                {
+                    employeeId: 2,
+                    name: 'Priya Singh',
+                    email: 'priya.singh@example.com',
+                    departmentId: 2,
+                    roleId: 2,
+                },
+                {
+                    employeeId: 3,
+                    name: 'Ankit Verma',
+                    email: 'ankit.verma@example.com',
+                    departmentId: 1,
+                    roleId: 3,
+                },
+            ]
+        };
+
+        getAllemployee.mockReturnValue(mockData)
+        let result = await request(server).get("/employees");
+        expect(result.status).toBe(200);
+        expect(result.body).toEqual(mockData);
+    });
+
+    it("/employees/details/:id should return employee by id", async () => {
+        const mockData = {
+            employee: {
+                employeeId: 1,
+                name: 'Rahul Sharma',
+                email: 'rahul.sharma@example.com',
+                departmentId: 1,
+                roleId: 1,
+            }
+        };
+
+        getEmployeeById.mockReturnValue(mockData)
+        let result = await request(server).get("/employees/details/1");
+        expect(result.status).toBe(200);
+        expect(result.body.employee).toEqual(mockData)
+    });
+});
+
+describe("validate functions", () => {
+    beforeEach(() => {
+        jest.clearAllMocks()
+    });
+
+    it("getAllemployee should return all employee data", async () => {
+        const mockData = [
+            {
+                employeeId: 1,
+                name: 'Rahul Sharma',
+                email: 'rahul.sharma@example.com',
+                departmentId: 1,
+                roleId: 1,
+            },
+            {
+                employeeId: 2,
+                name: 'Priya Singh',
+                email: 'priya.singh@example.com',
+                departmentId: 2,
+                roleId: 2,
+            },
+            {
+                employeeId: 3,
+                name: 'Ankit Verma',
+                email: 'ankit.verma@example.com',
+                departmentId: 1,
+                roleId: 3,
+            },
+        ];
+
+        getAllemployee.mockReturnValue(mockData);
+        let result = await getAllemployee();
+        expect(result).toEqual(mockData)
+    });
+});
